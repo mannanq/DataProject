64e2e077d3033a3373e904cd79cdd54db46e29c8
Mannan Qazi <mannanqazi2@gmail.com>
2018-07-31 14:34:35 -0400

    Added function for user input

--- height.py
+++ height.py
@@ -14,12 +14,13 @@ y = y.reshape(-1, 1)
 lm = LinearRegression()
 lm.fit(x,y)
 print('Int', lm.intercept_)
+print('Coef:', lm.coef_)
 plt.scatter(y, lm.predict(x))
 plt.xlabel("Actual Height")
 plt.ylabel("Predicted Height")
 plt.title("Actual vs Predicted Height")
-plt.show()
-lm.predict(x)[0]
+#plt.show()
+lm.predict(x)
 X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(x, y, test_size = 0.33, random_state = 5)
 print(X_train.shape)
 print(X_test.shape)
@@ -30,13 +31,31 @@ lm2 = LinearRegression()
 lm2.fit(X_train, Y_train)
 pred_train = lm2.predict(X_train)
 pred_test = lm2.predict(X_test)
+print("R squared:", lm2.score(X_test, Y_test))
 
 plt.scatter(lm2.predict(X_train), lm2.predict(X_train) - Y_train, c = 'b', alpha = 0.5)
 plt.scatter(lm2.predict(X_test), lm2.predict(X_test) - Y_test, c = 'g')
 plt.hlines(y = 0, xmin = 50, xmax = 80)
 plt.title("Residual Plot using training and test data")
 plt.ylabel("Residuals")
-plt.show()
+#plt.show()
+
+print(type(lm.intercept_))
+print(type(lm.coef_))
+intercept = float(lm.intercept_[0])
+print(intercept)
+coef = float(lm.coef_[0])
+print(coef)
+intercept.astype(int)
+coef.astype(int)
+
+
+#UserWe = input('Enter Weight:')
+def PredictHeight(UserWe):
+    Height = (coef * (UserWe)) + intercept
+    print('Predicted height of the person is:', Height)
+    
+#PredictHeight(UserWe)
 
 
 
